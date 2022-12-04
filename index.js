bce5878c7272a9d3efbe30068a18905a9169f863
Summry <nazira.fakhrurradi7@gmail.com>
2022-12-03 22:06:59 -0800

    Integrated express sessions, middlewares, configured express sessions, and added logout post route.

--- index.js
+++ index.js
@@ -1,27 +1,48 @@
 const express = require("express");
+const session = require("express-session");
 const app = express();
 const path = require("path");
 const ejsLayouts = require("express-ejs-layouts");
-const reminderController = require("./controller/reminder_controller");
-const authController = require("./controller/auth_controller");
+const reminderController = require("./controller/reminderController");
+const authController = require("./controller/authController");
+
+const { ensureAuthenticated, forwardAuthenticated } = require("./middleware/checkAuth");
 
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 
 app.use(ejsLayouts);
 
 app.set("view engine", "ejs");
 
+app.use(
+  session({
+    secret: "secret",
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      httpOnly: true,
+      secure: false,
+      maxAge: 24 * 60 * 60 * 1000,
+    },
+  })
+)
+
+app.use(express.json());
+const passport = require("./middleware/passport");
+app.use(passport.initialize());
+app.use(passport.session());
+
 // Routes start here
 
-app.get("/reminders", reminderController.list);
+app.get("/reminders", ensureAuthenticated, reminderController.list);
 
-app.get("/reminder/new", reminderController.new);
+app.get("/reminder/new", ensureAuthenticated, reminderController.new);
 
-app.get("/reminder/:id", reminderController.listOne);
+app.get("/reminder/:id", ensureAuthenticated, reminderController.listOne);
 
-app.get("/reminder/:id/edit", reminderController.edit);
+app.get("/reminder/:id/edit", ensureAuthenticated, reminderController.edit);
 
 app.post("/reminder/", reminderController.create);
 
@@ -32,10 +53,12 @@ app.post("/reminder/update/:id", reminderController.update);
 app.post("/reminder/delete/:id", reminderController.delete);
 
 // Fix this to work with passport! The registration does not need to work, you can use the fake database for this.
-app.get("/register", authController.register);
-app.get("/login", authController.login);
+app.get("/register", forwardAuthenticated, authController.register);
+app.get("/login", forwardAuthenticated, authController.login);
 app.post("/register", authController.registerSubmit);
 app.post("/login", authController.loginSubmit);
+
+app.post("/logout", authController.logoutSubmit);
 
 app.listen(3001, function () {
   console.log(
