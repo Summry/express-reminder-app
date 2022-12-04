32cc24e240ebbdc191fcae0461bb6cb37eb610be
Summry <nazira.fakhrurradi7@gmail.com>
2022-12-03 22:04:07 -0800

    Added user model functions for the fake database. Inserted more users and reminders into the fake database.

--- database.js
+++ database.js
@@ -1,17 +1,61 @@
-let Database = {
-    cindy: {
-        reminders: [
-            {
-                id: 1,
-                title: "abc",
-                description: "abcabc",
-                completed: false
-            }
-        ]
-    },
-    alex: {
-        reminders: []
-    }
-}
+const database = [
+	{
+		id: 1, // user id
+		name: "Cindy Well",
+		email: "cindy123@gmail.com",
+		password: "cindy123!",
+		reminders: [
+			{
+				id: 1, // reminder id
+				title: "abc",
+				description: "abcabc",
+				completed: false
+			},
+			{
+				id: 2, // reminder id
+				title: "cindy 2nd reminder",
+				description: "do something",
+				completed: false
+			}
+		]
+	},
+	{
+		id: 1, // user id
+		name: "Bob Smith",
+		email: "bob123@gmail.com",
+		password: "bob123!",
+		reminders: [
+			{
+				id: 1, // reminder id
+				title: "bob's 1st reminder",
+				description: "bob 1st description",
+				completed: false
+			},
+			{
+				id: 2, // reminder id
+				title: "bob's 2nd reminder",
+				description: "bob 2nd description",
+				completed: false
+			}
+		]
+	},
+];
 
-module.exports = Database;
+const userModel = {
+	findOne: (email) => {
+		const user = database.find((user) => user.email === email);
+		if (user) {
+			return user;
+		}
+		throw new Error(`Couldn't find user with email: ${email}`);
+	},
+	findById: (id) => {
+		const user = database.find((user) => user.id === id);
+		if (user) {
+			return user;
+		}
+		throw new Error(`Couldn't find user with id: ${id}`);
+	},
+};
+
+module.exports = { database, userModel };
